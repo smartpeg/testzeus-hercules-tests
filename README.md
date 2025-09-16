1c282eaa981319120d3514b89dffe9c90a902afd
FatiguedFrench <139716243+FatiguedFrench@users.noreply.github.com>
2025-09-16 14:06:24 +0200

    V1

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Se si usa WSL Debian modificare il punto di mount di /tmp. Avere almeno 60 GB di spazio libero.
 ## Preparazione all'uso
 - Esegui ./init.sh
+- Modificare `agents_llm_config.json` per includere la API key
 - Esegui testzeus-hercules per eseguire i test
--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 echo "Installo gli strumenti di build e le librerie di sviluppo"
 sudo apt update
-sudo apt install -y tk-dev tcl-dev build-essential zlib1g-dev libssl-dev libffi-dev libbz2-dev liblzma-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev wget curl
+sudo apt install -y python3-pip tk-dev tcl-dev build-essential zlib1g-dev libssl-dev libffi-dev libbz2-dev liblzma-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev wget curl
 
 echo "Installo Pyenv per gestire le versioni di Python"
 curl -fsSL https://pyenv.run | bash
@@ -10,6 +10,7 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
 echo 'eval "$(pyenv init - bash)"' >> ~/.profile
 PATH="$HOME/.pyenv/bin:$PATH"
+source ~/.profile
 
 echo "Installo Python 3.11.13 con pyenv"
 pyenv install 3.11.13
@@ -20,14 +21,14 @@ pyenv global 3.11.13
 echo "Python 3 e' ora Python3.11.13, per revertire pyenv global system"
 python3 --version
 
-echo "Installo pip"
-curl -sS https://bootstrap.pypa.io/get-pip.py | python3
-
 echo "Creo un ambiente virtuale"
 python3 -m venv .
 source bin/activate
 source .env
 
 echo "Installo i pacchetti necessari per TestZeus Hercules"
-pip3 install --break-system-packages testzeus-hercules playwright ag2[openai] openai
+pip3 install --break-system-packages testzeus-hercules
+pip3 install --upgrade --break-system-packages ag2[openai] openai
+
+pip3 install --break-system-packages playwright
 playwright install --with-deps
