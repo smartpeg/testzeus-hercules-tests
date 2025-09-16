e4dcf4ebf2ef74aed8121a884e4fb19677a73880
FatiguedFrench <139716243+FatiguedFrench@users.noreply.github.com>
2025-09-16 11:57:51 +0200

    Funzionante

---
+++ deps.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.11 python3.11-venv python3.11-dev python3.11-distutils -y
--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ source bin/activate
 
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.11
 
-pip3 install testzeus-hercules playwright ag2[openai]
+pip3 install --break-system-packages testzeus-hercules playwright ag2[openai] openai
 playwright install --with-deps
 
 source .env
